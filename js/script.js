cdb29351eca07664549c9f660a9a47707112a4cc
Ashiq <robiulislam.1711@gmail.com>
2023-11-08 12:24:24 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let question_plus=document.querySelectorAll(".question_plus")
+let question_detail_pera=document.querySelector(".question_detail_pera")
+
+question_plus.addEventListener('click',function(){
+    question_detail_pera.style.dispay="block"
+})
+
+
